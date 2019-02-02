dc89c29606343949082d08950004ab5f6434dffe
Neha Gokhale <nmg140130@utdallas.edu>
2019-02-01 21:46:53 -0500

    Hello World, Booleans, Numbers, Strings, Variables
    Hello World, Booleans, Numbers, Strings, Variables

---
+++ src/Variables/variables.rb
@@ -0,0 +1,16 @@
+# Variables - placeholder to store a value
+
+location = "Georgia"
+puts location # Georgia
+
+my_num = 40
+my_num + 10 # we're not storing this value anywhere!
+puts my_num # 40
+
+# To modify a variable's value, we need to assign it to variable again
+my_num += 10 # my_num = my_num + 10
+puts my_num # 50
+
+# Variables & comparison
+is_even = my_num % 2 == 0
+puts is_even # true
